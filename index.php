1cc6de803a426eb72e2a50e7dcca579bda5ce496
Cosmo Swarbreck <c.swarbreck@gmail.com>
2018-11-28 22:31:53 +0000

    added some more styles

--- index.php
+++ index.php
@@ -19,31 +19,33 @@ get_header(); ?>
 	<div class="masthead container-fluid">
 		<div class="contianer headline">
 			<h1>Le Mourre Du Bes</h1>
+			<div class="container" style="padding-left: 70px; padding-right: 70px">
+			<hr style=" border-bottom: 2px solid white; margin: 0 auto">
+			</div>
 		</div>
 		
 		<div class="container navigation">
   			<div class="row">
     			<div class="col-sm">
-      			<h3>Contact</h3>
+      			<h3 style="text-align: left">Contact</h3>
     			</div>
    		 		<div class="col-sm">
 				<h3>Interior</h3>
     			</div>
     			<div class="col-sm">
-				<h3>Exterior</h3>
+				<h3 style="text-align: right">Exterior</h3>
     			</div>
   			</div>
 		</div>
 	</div>
-	<div class="container content">
+	<div class="container px-auto content">
 		<div class="row">
 			<div class="col-sm-4 content-left">
 				<h2>History of the House</h2>
-				<br>
-				<p>In mi orci, auctor a justo sed, mattis tristique est. Sed tristique suscipit sodales. Maecenas vitae tellus a eros ultricies finibus. Nam blandit augue mauris, eu volutpat dui convallis at. Duis maximus leo rutrum, bibendum est vel, luctus ex. Sed nec odio ac orci euismod aliquet. Phasellus pellentesque interdum accumsan. Donec finibus sapien porta purus condimentum fermentum.</p>
+				<p class="left">In mi orci, auctor a justo sed, mattis tristique est. Sed tristique suscipit sodales. Maecenas vitae tellus a eros ultricies finibus. Nam blandit augue mauris, eu volutpat dui convallis at. Duis maximus leo rutrum, bibendum est vel, luctus ex. Sed nec odio ac orci euismod aliquet. Phasellus pellentesque interdum accumsan. Donec finibus sapien porta purus condimentum fermentum.</p>
 			</div>
 			<div class="col-sm-8 content-right">
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec nisi at massa convallis hendrerit. Nunc ultrices tellus et laoreet imperdiet. Quisque volutpat luctus nulla. Sed vitae sollicitudin urna, vel pretium est. Aenean nisi tellus, rutrum eleifend magna eu, rhoncus fermentum nunc. Nullam dapibus sem non odio auctor tempus. Suspendisse potenti. Donec at diam a arcu fermentum aliquam vel nec nisi. Nullam ut turpis viverra, tristique tellus ornare, rhoncus neque. Nulla pellentesque, dolor dignissim vestibulum dictum, diam arcu suscipit nibh, ut ullamcorper sem ante at turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam interdum metus semper porttitor feugiat. Nam magna lacus, vehicula dapibus vehicula vel, malesuada efficitur leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut eget semper purus. Phasellus posuere velit eu urna pulvinar dignissim.</p>
+			<p class="right">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec nisi at massa convallis hendrerit. Nunc ultrices tellus et laoreet imperdiet. Quisque volutpat luctus nulla. Sed vitae sollicitudin urna, vel pretium est. Aenean nisi tellus, rutrum eleifend magna eu, rhoncus fermentum nunc. Nullam dapibus sem non odio auctor tempus. Suspendisse potenti. Donec at diam a arcu fermentum aliquam vel nec nisi. Nullam ut turpis viverra, tristique tellus ornare, rhoncus neque. Nulla pellentesque, dolor dignissim vestibulum dictum, diam arcu suscipit nibh, ut ullamcorper sem ante at turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam interdum metus semper porttitor feugiat. Nam magna lacus, vehicula dapibus vehicula vel, malesuada efficitur leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut eget semper purus. Phasellus posuere velit eu urna pulvinar dignissim.</p>
 			</div>
 		</div>
 	</div>		
